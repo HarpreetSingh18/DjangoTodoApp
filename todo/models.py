8e4902c3723e093824957c93a271af171bc63043
HarpreetSingh18 <hdhillon018@gmail.com>
2021-11-25 22:31:26 +0530

    completed app

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
 class TodoItem(models.Model):
     title=models.CharField(max_length=100)
     is_completed = models.BooleanField(default=False)
